1d2a6d989264624c56cf9d65cd4bf845caf9bd85
Gaya Kessler <gaya@theclevernode.com>
2024-05-13 15:19:47 +0200

    generate a plain old plain world

---
+++ src/main/java/nl/clevernode/mcgolddiggers/GoldDiggerWorld.java
@@ -0,0 +1,36 @@
+package nl.clevernode.mcgolddiggers;
+
+import org.bukkit.*;
+import org.bukkit.entity.Player;
+import org.bukkit.generator.ChunkGenerator;
+
+public class GoldDiggerWorld {
+
+    private final String worldName = "MCGoldDiggers";
+
+    private World getWorld() {
+        return Bukkit.getWorld(this.worldName);
+    }
+
+    public void initialize() {
+        // unload current world
+        Bukkit.getServer().unloadWorld(this.getWorld(), false);
+
+        // initialize the flat world
+        Bukkit.createWorld(
+                new WorldCreator(this.worldName)
+                        .type(WorldType.FLAT)
+                        .generateStructures(false)
+                        .generatorSettings("{ \"layers\": [{\"block\": \"stone\", \"height\": 1}, {\"block\": \"grass_block\", \"height\": 1}], \"biome\":\"plains\" }")
+        );
+    }
+
+    public Location getStandardLocation() {
+        return new Location(this.getWorld(), 0, -60, 0);
+    }
+
+    public void sendToStandardLocation(Player player) {
+        player.teleport(this.getStandardLocation());
+    }
+
+}
--- src/main/java/nl/clevernode/mcgolddiggers/MCGoldDiggers.java
+++ src/main/java/nl/clevernode/mcgolddiggers/MCGoldDiggers.java
@@ -1,6 +1,5 @@
 package nl.clevernode.mcgolddiggers;
 
-import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,14 +9,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MCGoldDiggers extends JavaPlugin implements Listener {
 
-    private String worldName = "leuke_naam";
+    GoldDiggerWorld world = new GoldDiggerWorld();
 
     @Override
     public void onEnable() {
         getLogger().info("MCGoldDiggers is aangezet!");
 
-        // initialize the flat world
-        Bukkit.createWorld(new WorldCreator(this.worldName).type(WorldType.FLAT));
+        this.world.initialize();
 
         // register event listeners
         getServer().getPluginManager().registerEvents(this, this);
@@ -31,20 +29,12 @@ public final class MCGoldDiggers extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        this.sendToStandardWorld(player);
+        this.world.sendToStandardLocation(player);
     }
 
     @EventHandler
     public void onRespawn(PlayerRespawnEvent event) {
-        event.setRespawnLocation(this.getStandardLocation());
+        event.setRespawnLocation(this.world.getStandardLocation());
     }
 
-    private Location getStandardLocation() {
-        World world = Bukkit.getWorld(this.worldName);
-        return new Location(world, 0, 0, 0);
-    }
-
-    private void sendToStandardWorld(Player player) {
-        player.teleport(this.getStandardLocation());
-    }
 }
